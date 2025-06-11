d81c2c8b5d5b6f35931810a7f77d7879207aed80
afloresmtz <a01708363@tec.mx>
2025-06-10 20:33:44 -0600

    Update Task.cpp

--- classes/Task.cpp
+++ classes/Task.cpp
@@ -1,1 +1,34 @@
+#include "Task.h"
+#include "Date.h"
+#include "Subject.h"
+
+#include <string>
+
+Task::Task(std::string description, Date* dueDate, Subject* subject) {
+    this->description = description;
+    this->dueDate = dueDate;
+    this->subject = subject;
+}
+
+std::string Task::getDescription() {
+    return description;
+}
+void Task::setDescription(std::string newDescription) {
+    description = newDescription;
+}
+
+Date Task::getDueDate() {
+    return *dueDate;
+}
+void Task::setDueDate(Date* newDate) {
+    dueDate = newDate;
+}
+
+Subject Task::getSubject() {
+    return *subject;
+}
+
+void Task::setSubject(Subject* newSubject) {
+    subject = newSubject;
+}
 
