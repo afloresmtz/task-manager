4207205d5770b59e2ee73e0b6510ba2d3ae03451
afloresmtz <a01708363@tec.mx>
2025-06-10 20:30:54 -0600

    Update NormalTask.h

--- classes/NormalTask.h
+++ classes/NormalTask.h
@@ -1,16 +1,19 @@
 #ifndef NORMALTASK_H
 #define NORMALTASK_H
 
-#include <Task.h>
+#include "Task.h"
 #include <string>
 
 class NormalTask : public Task {
 private:
+    int progress;
     int urgencyLevel;
 public:
-    NormalTask(std::string description, Date dueDate, int progress, Subject subject, int urgency);
+    NormalTask(std::string description, Date* dueDate, Subject* subject, int progress, int urgency);
 
-    std::string showDetail() override;
+    std::string showDetail() override; // overridden method from task
+    int getProgress();
+    void setProgress(int newProgress);
     int getUrgency();
     void setUrgency(int newUrgency);
 };
