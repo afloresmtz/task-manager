af750a37a57285b929b100be730ce2e89037b755
afloresmtz <a01708363@tec.mx>
2025-06-10 20:29:33 -0600

    Update Date.h

--- classes/Date.h
+++ classes/Date.h
@@ -5,21 +5,22 @@
 
 class Date {
 private:
-    int day[3];
-    int time[3];
+    int day[3]; // day, month, year
+    std::string time[3]; // hour, minute, second
 public:
-    Date(int day[3]);
-    Date(int day[3], int time[3]);
+    Date(int day[3], std::string time[3]);
 
     std::string printDate();
 
     int getYear();
     int getMonth();
     int getDay();
-    int getHour();
-    int getMinute();
-    int getSecond();
-    
+    std::string getHour();
+    std::string getMinute();
+    std::string getSecond();
+
+    bool operator==(Date* otherDate); // to check if dates are the same
+
 };
 
 #endif
