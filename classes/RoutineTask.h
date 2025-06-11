42db12d7ad5f8e064dbc6b56711470d4c8b0442e
afloresmtz <a01708363@tec.mx>
2025-06-10 20:31:49 -0600

    Update RoutineTask.h

--- classes/RoutineTask.h
+++ classes/RoutineTask.h
@@ -1,22 +1,24 @@
 #ifndef ROUTINETASK_H
 #define ROUTINETASK_H
 
-#include <Task.h>
+#include "Task.h"
+#include "Date.h"
+#include "Subject.h"
 #include <string>
 
 class RoutineTask : public Task {
 private:
-    int routineType;
+    std::string routineType;
     int repetitions;
 public:
-    RoutineTask(std::string description, int progress, Subject subject, int routine, int repeat);
+    RoutineTask(std::string description, Date* dueDate, Subject* subject, std::string routine, int repeat);
 
-    std::string showDetail() override;
-    int getRoutineType();
-    void setRoutineType(int type);
+    std::string showDetail() override; // overridden method from Task
+    std::string getRoutineType();
+    void setRoutineType(std::string type);
     int getRepetitions();
     void setRepetitions(int amount);
 
-}
+};
 
 #endif
