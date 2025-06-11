04ea0688f3f4ef098775b92a364bb43e8e2b9dee
afloresmtz <a01708363@tec.mx>
2025-06-10 20:34:21 -0600

    Update Task.h

--- classes/Task.h
+++ classes/Task.h
@@ -8,25 +8,22 @@
 class Task {
 protected:
     std::string description;
-    Date dueDate;
-    int progress;
-    Subject subject;
+    Date* dueDate;
+    Subject* subject;
 public:
-    Task(std::string description, Date dueDate, int progress, Subject subject);
+    Task(std::string description, Date* dueDate, Subject* subject);
 
-    virtual std::string showDetail() = 0;
-    
+    virtual std::string showDetail() = 0; // abstract class definition here
+                                          // this way no objects of the Task class can be created
+
     std::string getDescription();
     void setDescription(std::string newDescription);
 
     Date getDueDate();
-    void setDueDate(Date newDate);
-
-    int getProgress();
-    void setProgress(int newProgress);
+    void setDueDate(Date* newDate);
 
     Subject getSubject();
-    void setSubject(Subject newSubject);
+    void setSubject(Subject* newSubject);
 
 };
 
